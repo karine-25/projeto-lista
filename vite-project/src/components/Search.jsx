3ba845505d6e6220e36375bbedf88b569323aca5
karine-25 <karine.pereira@gsd.com.br>
2023-12-26 14:46:52 -0300

    Lista

---
+++ vite-project/src/components/Search.jsx
@@ -0,0 +1,13 @@
+const Search = ({search, setSearch}) => {
+  return (
+    <div className="search">
+        <h1>Pesquisar:</h1>
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}
+       placeholder ={'Digite para pesquisar...'}/>
+
+
+    </div>
+  )
+}
+
+export default Search
